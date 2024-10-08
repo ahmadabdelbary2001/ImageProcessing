a5a59382613b88864b7a7a5c972db7d79c277343
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2024-10-09 00:54:22 +0300

    final

--- src/main/java/org/example/HorizontalImageProcessor.java
+++ src/main/java/org/example/HorizontalImageProcessor.java
@@ -1,5 +1,31 @@
 package org.example;
 
-public class HorizontalImageProcessor  implements ImageProcessor {
+import java.awt.image.BufferedImage;
 
+public class HorizontalImageProcessor  implements ImageProcessor {
+    @Override
+    public void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int numberOfThreads) throws InterruptedException {
+        int width = originalImage.getWidth();
+        int heightPerThread = originalImage.getHeight() / numberOfThreads;
+
+        Thread[] threads = new Thread[numberOfThreads];
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            final int threadIndex = i;
+
+            threads[i] = new Thread(() -> {
+                int yOrigin = heightPerThread * threadIndex;
+                RecolorPixels recolorPixels = new RecolorPixels(originalImage, resultImage, 0, yOrigin, width, heightPerThread);
+                recolorPixels.recolorImage();
+            });
+
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            if (thread != null) {
+                thread.join();
+            }
+        }
+    }
 }
